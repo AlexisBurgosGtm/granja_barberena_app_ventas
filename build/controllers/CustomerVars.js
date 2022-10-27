115b97628ae8b3c392efe5967b3208e86a1fb215
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2022-10-26 23:53:55 -0600

    inicial

---
+++ build/controllers/CustomerVars.js
@@ -0,0 +1,12 @@
+let GlobalToken = '8813591-8';
+let GlobalCodSucursal = '';
+
+let GlobalEmpnit = '8813591-8';
+let GlobalEmpNombre = 'SANTA FE';
+
+
+let GlobalVentaMinima = 79.99;
+
+let dataEmpresas = [ 
+    {codsucursal:"8813591-8",nomsucursal:"SANTA FE",color:"success"}
+  ];
