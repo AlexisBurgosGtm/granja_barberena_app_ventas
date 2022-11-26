bfe04dde00121e104d1d1324f9c44f77d645b667
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2022-11-25 20:16:38 -0600

    va

--- build/views/login/index.js
+++ build/views/login/index.js
@@ -64,7 +64,7 @@ function getView(){
                             </div>
 
                             <div class="form-group" align="right">  
-                                <small class="text-secondary">v1.13</small>
+                                <small class="text-secondary">v1.14</small>
                                 <br>
                                 <small>
                                     <!--
