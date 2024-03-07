85c18784ce06d16a192d043fdc711d2414c15355
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2024-03-07 12:17:20 -0600

    Update sw.js

--- build/sw.js
+++ build/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'pre-cache-v2.2';
-const dynamicCacheName = 'runtime-cache-v2.2';
+const staticCacheName = 'pre-cache-v2.3';
+const dynamicCacheName = 'runtime-cache-v2.3';
 
 // Pre Caching Assets
 const precacheAssets = [
